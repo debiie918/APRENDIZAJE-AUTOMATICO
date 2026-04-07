3f59288731a3736b50d0dd1232fb8c7ac312f603
debiie918 <deboracalisaya@gmail.com>
2026-04-07 10:51:16 -0300

    OK

---
+++ clase1/arrayptyhon.py
@@ -0,0 +1,4 @@
+array_ceros = [0] * 10
+
+print("Array de 10 ceros:")
+print(array_ceros)
---
+++ clase1/ejercicio3.py
@@ -0,0 +1,5 @@
+#Crea un array con los números pares del 2 al 50
+array_pares = list(range(2, 51, 2))
+print("Array con números pares del 2 al 50:")
+print(array_pares)
+
